07b4572fce4aa4d5553f7b51b9ab8fc393dbe58c
Istvan <17463906+purple0CaT@users.noreply.github.com>
2022-02-05 20:41:39 +0000

    some addition fix

--- backend/src/auth/controller/auth.controller.ts
+++ backend/src/auth/controller/auth.controller.ts
@@ -11,6 +11,7 @@ export class AuthController {
   register(@Body() user: User): Observable<User> {
     return this.authService.registerAccount(user);
   }
+  // 
   @Post('login')
   login(@Body() user: User): Observable<{ token: string }> {
     return this.authService
